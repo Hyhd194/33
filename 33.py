c548b787a3355332affa82986eb2ace73656e83f
Hyhd194 <1445023413@qq.com>
2025-05-08 18:59:35 +0800

    Update 33.py

--- 33.py
+++ 33.py
@@ -1,1 +1,12 @@
-print222
+import socket
+import threading
+
+class TupleSpaceServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.tuple_space = {}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print(f"Server listening on {self.host}:{self.port}")
